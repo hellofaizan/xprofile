606e06857d3b3373899ac15babd68d676fb36d7c
Mohammad Faizan <faizancurious@gmail.com>
2023-07-29 17:56:27 +0530

    User data API

--- src/pages/api/users.js
+++ src/pages/api/users.js
@@ -1,5 +1,8 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 
 export default function handler(req, res) {
-  res.status(200).json({ name: 'John Doe' })
+  // read users from a local JSON file
+  const users = require('/data/users.json')
+  res.status(200).json(users)
+
 }
