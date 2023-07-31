3a2084ed6bf5caea845bb48ebd477c022ab62956
IkhlaqMalik <ikhlaqmalik.si@gmail.com>
2023-07-31 14:20:15 +0530

    - Fixed the dismiss button functionality - ikhlaq

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,12 +8,13 @@ import Card from '../components/Card'
 
 const Home = () => {
   const [search, setSearch] = useState(false)
+  const [showControDiv, setShowControDiv] = useState(true)
   const fetcher = (...args) => fetch(...args).then(res => res.json())
 
   const { data, error } = useSWR('/api/users', fetcher)
 
   const dismiss = () => {
-    document.querySelector('.banner').classList.add('hidden')
+    setShowControDiv(false)
   }
 
   if (error) return <>
@@ -166,28 +167,31 @@ const Home = () => {
             </div>
           )}
 
-          <div className='banner hidden md:block fixed inset-x-0 bottom-0'>
-            <div className="flex items-center justify-between gap-4 bg-white/5 px-4 py-3 text-white" >
-              <p className="text-sm font-medium">
-                Developed with <i className="bi bi-heart-fill text-red-500"></i> by <Link href="https://hellofaizan.me" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-500 cursor-pointer">@hellofaizan</Link> ( Open Source )
-              </p>
+          {
+            showControDiv ? (
+              <div className='banner hidden md:block fixed inset-x-0 bottom-0'>
+                <div className="flex items-center justify-between gap-4 bg-white/5 px-4 py-3 text-white" >
+                  <p className="text-sm font-medium">
+                    Developed with <i className="bi bi-heart-fill text-red-500"></i> by <Link href="https://hellofaizan.me" target="_blank" rel="noopener noreferrer" className="text-blue-400 hover:text-blue-500 cursor-pointer">@hellofaizan</Link> ( Open Source )
+                  </p>
 
-              <button aria-label="Dismiss" onClick={dismiss} className="shrink-0 rounded-lg bg-black/10 p-1 transition hover:bg-white/10">
-                <svg
-                  xmlns="http://www.w3.org/2000/svg"
-                  className="h-5 w-5"
-                  viewBox="0 0 20 20"
-                  fill="currentColor">
-                  <path
-                    fill-rule="evenodd"
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clip-rule="evenodd"
-                  />
-                </svg>
-              </button>
-            </div>
-          </div>
-
+                  <button aria-label="Dismiss" onClick={dismiss} className="shrink-0 rounded-lg bg-black/10 p-1 transition hover:bg-white/10">
+                    <svg
+                      xmlns="http://www.w3.org/2000/svg"
+                      className="h-5 w-5"
+                      viewBox="0 0 20 20"
+                      fill="currentColor">
+                      <path
+                        fill-rule="evenodd"
+                        d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
+                        clip-rule="evenodd"
+                      />
+                    </svg>
+                  </button>
+                </div>
+              </div>
+            ) : ""
+          }
 
         </motion.div>
       </>
