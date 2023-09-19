3c07f89fb2af5db933529b6caea7fad93290a4ae
rahulpandey70 <rahulpandey9103@gmail.com>
2023-09-19 08:45:45 +0530

    fix: incorrect word

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -9,81 +9,81 @@ import ThemeProvider from "@/ThemeProvider";
 // 2. create a custom theme
 
 export default function App({ Component, pageProps }) {
-  return (
-    <>
-      <Head>
-        <meta charSet="utf-8" />
-        <title>𝕏 Profile - Your Twitter Profile Card</title>
-        <link rel="icon" href="/x_large.png" />
-        <meta name="viewport" content="width=device-width,initial-scale=1" />
-        <meta name="theme-color" content="#171717" />
-        <meta
-          name="keywords"
-          content="X, Twitter, Twitter Profile, X Profile Card, Open Source, github, typescript, nextjs, hellofaizan portfolio"
-        />
-        <meta name="description" content="Showcase your 𝕏 profile to world" />
-        <meta name="author" content="Hello Faizan" />
-        <link rel="apple-touch-icon" href="/x_large.png" />
+	return (
+		<>
+			<Head>
+				<meta charSet="utf-8" />
+				<title>𝕏 Profile - Your Twitter Profile Card</title>
+				<link rel="icon" href="/x_large.png" />
+				<meta name="viewport" content="width=device-width,initial-scale=1" />
+				<meta name="theme-color" content="#171717" />
+				<meta
+					name="keywords"
+					content="X, Twitter, Twitter Profile, X Profile Card, Open Source, github, typescript, nextjs, hellofaizan portfolio"
+				/>
+				<meta name="description" content="Showcase your 𝕏 profile to world" />
+				<meta name="author" content="Hello Faizan" />
+				<link rel="apple-touch-icon" href="/x_large.png" />
 
-        <meta
-          property="og:title"
-          content="𝕏 Profile - Your Twitter Profile Card"
-        />
-        <meta
-          property="og:site_name"
-          content="Showcase your 𝕏 profile to world"
-        />
-        <meta property="og:url" content="https://xprofile.hellofaizan.me" />
+				<meta
+					property="og:title"
+					content="𝕏 Profile - Your Twitter Profile Card"
+				/>
+				<meta
+					property="og:site_name"
+					content="Showcase your 𝕏 profile to world"
+				/>
+				<meta property="og:url" content="https://xprofile.hellofaizan.me" />
 
-        <meta
-          property="og:description"
-          content="Showcase your 𝕏 profile to world and get recomendations from community."
-        />
+				<meta
+					property="og:description"
+					content="Showcase your 𝕏 profile to world and get recommendations from community."
+				/>
 
-        <meta
-          property="og:image"
-          itemProp="image"
-          content="https://i.insider.com/64be308bed4f4600196198a0?width=1200&format=jpeg"
-        />
+				<meta
+					property="og:image"
+					itemProp="image"
+					content="https://i.insider.com/64be308bed4f4600196198a0?width=1200&format=jpeg"
+				/>
 
-        <meta property="og:type" content="article" />
-        <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:site" content="@HelloFaizaan" />
-        <meta name="twitter:creator" content="@HelloFaizaan" />
-        <meta
-          name="twitter:title"
-          content="𝕏 Profile - Your Twitter Profile Card"
-        />
-        <meta
-          name="twitter:description"
-          content="Showcase your 𝕏 profile to world and get recomendations from community."
-        />
-        <meta
-          name="twitter:image"
-          itemProp="image"
-          content="https://i.insider.com/64be308bed4f4600196198a0?width=1200&format=jpeg"
-        />
-      </Head>
+				<meta property="og:type" content="article" />
+				<meta name="twitter:card" content="summary_large_image" />
+				<meta name="twitter:site" content="@HelloFaizaan" />
+				<meta name="twitter:creator" content="@HelloFaizaan" />
+				<meta
+					name="twitter:title"
+					content="𝕏 Profile - Your Twitter Profile Card"
+				/>
+				<meta
+					name="twitter:description"
+					content="Showcase your 𝕏 profile to world and get recommendations from community."
+				/>
+				<meta
+					name="twitter:image"
+					itemProp="image"
+					content="https://i.insider.com/64be308bed4f4600196198a0?width=1200&format=jpeg"
+				/>
+			</Head>
 
-      <ThemeProvider>
-        <Script
-          strategy="afterInteractive"
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-GV02M4EWW9"
-        ></Script>
+			<ThemeProvider>
+				<Script
+					strategy="afterInteractive"
+					async
+					src="https://www.googletagmanager.com/gtag/js?id=G-GV02M4EWW9"
+				></Script>
 
-        <Script strategy="afterInteractive" id="google-analytics">
-          {`
+				<Script strategy="afterInteractive" id="google-analytics">
+					{`
              window.dataLayer = window.dataLayer || [];
              function gtag(){dataLayer.push(arguments);}
              gtag('js', new Date());
 
              gtag('config', 'G-GV02M4EWW9');
           `}
-        </Script>
-        <Component {...pageProps} />
-        <Analytics />
-      </ThemeProvider>
-    </>
-  );
+				</Script>
+				<Component {...pageProps} />
+				<Analytics />
+			</ThemeProvider>
+		</>
+	);
 }
