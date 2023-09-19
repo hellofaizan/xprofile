525f63be3d77131aea9ae80817e27502b466a44f
rahulpandey70 <rahulpandey9103@gmail.com>
2023-09-19 22:20:37 +0530

    change class to className

--- src/components/Spinner.js
+++ src/components/Spinner.js
@@ -1,10 +1,10 @@
 const Spinner = () => {
 	return (
-		<div class="text-center">
+		<div className="text-center">
 			<div role="status">
 				<svg
 					aria-hidden="true"
-					class="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
+					className="inline w-8 h-8 mr-2 text-gray-200 animate-spin dark:text-gray-600 fill-blue-600"
 					viewBox="0 0 100 101"
 					fill="none"
 					xmlns="http://www.w3.org/2000/svg"
@@ -18,7 +18,7 @@ const Spinner = () => {
 						fill="currentFill"
 					/>
 				</svg>
-				<span class="sr-only">Loading...</span>
+				<span className="sr-only">Loading...</span>
 			</div>
 		</div>
 	);
